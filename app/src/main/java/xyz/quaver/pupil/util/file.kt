1a7e2dc2d090d19d9a3ea5247e2c27281f196bec
tom5079 <tom5079@naver.com>
2019-06-23 00:23:17 +0900

    Migrate to Android 29 Re-Added Cache clear to prevent deleting downloading images

--- app/src/main/java/xyz/quaver/pupil/util/file.kt
+++ app/src/main/java/xyz/quaver/pupil/util/file.kt
@@ -2,10 +2,11 @@ package xyz.quaver.pupil.util
 
 import android.content.Context
 import android.os.Environment
+import android.provider.MediaStore
 import java.io.File
 
 fun getCachedGallery(context: Context, galleryID: Int): File {
-    return File(Environment.getExternalStorageDirectory(), "Pupil/$galleryID").let {
+    return File(context.getExternalFilesDir("Pupil"), galleryID.toString()).let {
         when {
             it.exists() -> it
             else -> File(context.cacheDir, "imageCache/$galleryID")
