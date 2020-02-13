842148647f4b720e08bb7148c03314d0bd486032
Pupil <tom5079@naver.com>
2020-02-13 19:42:25 +0900

    Changed to log fetchGallery exceptions

--- libpupil/src/main/java/xyz/quaver/hitomi/galleryblock.kt
+++ libpupil/src/main/java/xyz/quaver/hitomi/galleryblock.kt
@@ -34,35 +34,31 @@ fun fetchNozomi(area: String? = null, tag: String = "index", language: String = 
                 else -> "$protocol//$domain/$area/$tag-$language$nozomiextension"
             }
 
-    try {
-        with(URL(url).openConnection() as HttpsURLConnection) {
-            requestMethod = "GET"
+    with(URL(url).openConnection() as HttpsURLConnection) {
+        requestMethod = "GET"
 
-            if (start != -1 && count != -1) {
-                val startByte = start*4
-                val endByte = (start+count)*4-1
+        if (start != -1 && count != -1) {
+            val startByte = start*4
+            val endByte = (start+count)*4-1
 
-                setRequestProperty("Range", "bytes=$startByte-$endByte")
-            }
-
-            connect()
-
-            val totalItems = getHeaderField("Content-Range")
-                .replace(Regex("^[Bb]ytes \\d+-\\d+/"), "").toInt() / 4
-
-            val nozomi = ArrayList<Int>()
-
-            val arrayBuffer = ByteBuffer
-                .wrap(inputStream.readBytes())
-                .order(ByteOrder.BIG_ENDIAN)
-
-            while (arrayBuffer.hasRemaining())
-                nozomi.add(arrayBuffer.int)
-
-            return Pair(nozomi, totalItems)
+            setRequestProperty("Range", "bytes=$startByte-$endByte")
         }
-    } catch (e: Exception) {
-        return Pair(emptyList(), 0)
+
+        connect()
+
+        val totalItems = getHeaderField("Content-Range")
+            .replace(Regex("^[Bb]ytes \\d+-\\d+/"), "").toInt() / 4
+
+        val nozomi = ArrayList<Int>()
+
+        val arrayBuffer = ByteBuffer
+            .wrap(inputStream.readBytes())
+            .order(ByteOrder.BIG_ENDIAN)
+
+        while (arrayBuffer.hasRemaining())
+            nozomi.add(arrayBuffer.int)
+
+        return Pair(nozomi, totalItems)
     }
 }
 
@@ -82,30 +78,26 @@ data class GalleryBlock(
 fun getGalleryBlock(galleryID: Int) : GalleryBlock? {
     val url = "$protocol//$domain/$galleryblockdir/$galleryID$extension"
 
-    try {
-        val doc = Jsoup.connect(url).get()
+    val doc = Jsoup.connect(url).get()
 
-        val galleryUrl = doc.selectFirst(".lillie").attr("href")
+    val galleryUrl = doc.selectFirst(".lillie").attr("href")
 
-        val thumbnails = doc.select("img").map { protocol + it.attr("data-src") }
+    val thumbnails = doc.select("img").map { protocol + it.attr("data-src") }
 
-        val title = doc.selectFirst("h1.lillie > a").text()
-        val artists = doc.select("div.artist-list a").map{ it.text() }
-        val series = doc.select("a[href~=^/series/]").map { it.text() }
-        val type = doc.selectFirst("a[href~=^/type/]").text()
+    val title = doc.selectFirst("h1.lillie > a").text()
+    val artists = doc.select("div.artist-list a").map{ it.text() }
+    val series = doc.select("a[href~=^/series/]").map { it.text() }
+    val type = doc.selectFirst("a[href~=^/type/]").text()
 
-        val language = {
-            val href = doc.select("a[href~=^/index-.+-1.html]").attr("href")
-            href.slice(7 until href.indexOf("-1"))
-        }.invoke()
+    val language = {
+        val href = doc.select("a[href~=^/index-.+-1.html]").attr("href")
+        href.slice(7 until href.indexOf("-1"))
+    }.invoke()
 
-        val relatedTags = doc.select(".relatedtags a").map {
-            val href = URLDecoder.decode(it.attr("href"), "UTF-8")
-            href.slice(5 until href.indexOf("-all"))
-        }
-
-        return GalleryBlock(Code.HITOMI, galleryID, galleryUrl, thumbnails, title, artists, series, type, language, relatedTags)
-    } catch (e: Exception) {
-        return null
+    val relatedTags = doc.select(".relatedtags a").map {
+        val href = URLDecoder.decode(it.attr("href"), "UTF-8")
+        href.slice(5 until href.indexOf("-all"))
     }
+
+    return GalleryBlock(Code.HITOMI, galleryID, galleryUrl, thumbnails, title, artists, series, type, language, relatedTags)
 }
