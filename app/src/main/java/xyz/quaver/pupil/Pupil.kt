8f3dcc72ba6ab123d840333c450a5d98ba8d1942
tom5079 <tom5079@naver.com>
2020-12-27 19:36:23 +0900

    SAF

--- app/src/main/java/xyz/quaver/pupil/Pupil.kt
+++ app/src/main/java/xyz/quaver/pupil/Pupil.kt
@@ -26,6 +26,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
