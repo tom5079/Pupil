c9cc52515c5e7848f4ba89868c3561a73f2b65c1
tom5079 <tom5079@naver.com>
2021-07-24 12:41:26 +0900

    Migrate to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("org.jetbrains.kotlin:kotlin-android-extensions:1.5.21")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
+        classpath("com.google.gms:google-services:4.3.8")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
+        classpath("com.google.firebase:perf-plugin:1.4.0")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
