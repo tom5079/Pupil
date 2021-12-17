2df4f43c445aaef72beacc42049517801492a837
tom5079 <tom5079@naver.com>
2021-12-17 11:56:34 +0900

    Changed Theme / Deprecated Preferences. Use DataStore instead.

--- app/src/main/java/xyz/quaver/pupil/ui/composable/ProgressCard.kt
+++ app/src/main/java/xyz/quaver/pupil/ui/composable/ProgressCard.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -20,7 +21,7 @@ fun ProgressCard(progress: Float? = null, content: @Composable () -> Unit) {
         elevation = 4.dp
     ) {
         Column {
-            progress?.run { LinearProgressIndicator(progress = progress, modifier = Modifier.fillMaxWidth()) }
+            progress?.run { LinearProgressIndicator(progress = progress, modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colors.secondary) }
             content()
         }
     }
