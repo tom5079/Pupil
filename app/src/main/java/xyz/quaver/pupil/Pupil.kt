53b74f0b8d87a0cc4584cc00d5116b9de5ec6748
tom5079 <tom5079@naver.com>
2022-01-03 00:00:37 +0900

    User-Agent

--- app/src/main/java/xyz/quaver/pupil/Pupil.kt
+++ app/src/main/java/xyz/quaver/pupil/Pupil.kt
@@ -109,7 +109,11 @@ class Pupil : Application() {
             .readTimeout(0, TimeUnit.SECONDS)
             .proxyInfo(proxyInfo)
             .addInterceptor { chain ->
-                val request = chain.request()
+                val request = chain.request().newBuilder()
+                    .header("User-Agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+                            "Ubuntu Chromium/70.0.3538.77 Chrome/70.0.3538.77 Safari/537.36")
+                    .build()
+
                 val tag = request.tag() ?: return@addInterceptor chain.proceed(request)
 
                 interceptors[tag::class]?.invoke(chain) ?: chain.proceed(request)
