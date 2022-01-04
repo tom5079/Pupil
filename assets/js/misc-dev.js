7727589ee791d09b589db7adf663f660999621c8
tom5079 <tom5079@naver.com>
2022-01-04 18:35:20 +0900

    test autorefresh

--- assets/js/misc-dev.js
+++ assets/js/misc-dev.js
@@ -1,6 +1,6 @@
 function get_gallery_info(galleryID) {
     return new Promise((resolve, reject) => {
-        $.getScript(`https://ltn.hitomi.la/galleries/${galleryID}.html`, () => {
+        $.getScript(`https://ltn.hitomi.la/galleryblock/${galleryID}.html`, () => {
             resolve(galleryinfo);
         });
     });
