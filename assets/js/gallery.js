208abfe127567dc84ee80dd04b1d1ff0ee52e836
tom5079 <tom5079@naver.com>
2022-01-20 15:58:59 +0900

    0.0.6 publish hitomi.html

---
+++ assets/js/gallery.js
@@ -0,0 +1,58 @@
+async function get_gallery_url(galleryid) {
+    const url = `https://hitomi.la/galleries/${galleryid}.html`;
+
+    const response = await fetch(url);
+    const html = await response.text();
+
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+
+    return doc.querySelector('link').getAttribute('href');
+}
+
+async function get_gallery(galleryid) {
+    const url = await get_gallery_url(galleryid);
+
+    const response = await fetch(url);
+    const html = await response.text();
+
+    const doc = new DOMParser().parseFromString(rewrite_tn_paths(html), 'text/html');
+
+    //related
+    eval(Array.from(doc.getElementsByTagName('script')).find(elm => elm.innerHTML.includes('var related')).innerHTML);
+
+    const lang_list = {};
+
+    Array.from(doc.querySelectorAll('#lang-list a')).forEach(elm => lang_list[elm.innerText] = elm.getAttribute('href').slice(11, -5));
+
+    const cover = replace_jpg_tn(doc.querySelector('.cover img').src);
+
+    const title = doc.querySelector('.gallery h1 a').innerText;
+    const artists = Array.from(doc.querySelectorAll('.gallery h2 a'), elm => elm.innerText);
+    const groups = Array.from(doc.querySelectorAll('.gallery-info a[href^="/group."]'), elm => elm.innerText);
+    const type = doc.querySelector('.gallery-info a[href^="/type/"]').innerText.trim();
+
+    const language = doc.querySelector('.gallery-info a[href^="/index"][href$=".html"]').getAttribute('href').slice(7, -5);
+
+    const series = Array.from(doc.querySelectorAll('.gallery-info a[href^="/series/"]'), elm => elm.innerText);
+    const characters = Array.from(doc.querySelectorAll('.gallery-info a[href^="/character/"]'), elm => elm.innerText);
+
+    const tags = Array.from(doc.querySelectorAll('.gallery-info a[href^="/tag/"]'), elm => decodeURIComponent(elm.getAttribute('href')).slice(5, -9));
+
+    const gallery_info = await get_gallery_info(galleryid);
+    const thumbnails = gallery_info.files.map(file => url_from_url_from_hash(galleryid, file, 'webpsmallsmalltn', 'webp', 'tn'));
+
+    return {
+        related: related,
+        langList: lang_list,
+        cover: cover,
+        title: title,
+        artists: artists,
+        groups: groups,
+        type: type,
+        language: language,
+        series: series,
+        characters: characters,
+        tags: tags,
+        thumbnails: thumbnails
+    };
+}
--- assets/js/misc.js
+++ assets/js/misc.js
@@ -5,3 +5,67 @@ function get_gallery_info(galleryID) {
         });
     });
 }
+
+function do_search(query) {
+    let terms = query.toLowerCase().trim().split(/\s+/);
+    let positive_terms = [], negative_terms = [];
+
+    $.each(terms, function (i, term) {
+        term = term.replace(/_/g, ' ');
+        if (term.match(/^-/)) {
+            negative_terms.push(term.replace(/^-/, ''));
+        } else {
+            positive_terms.push(term);
+        }
+    });
+
+
+    return new Promise((resolve, reject) => { //first results
+        if (!positive_terms.length) {
+            get_galleryids_from_nozomi(undefined, 'index', 'all').then(results => {
+                resolve(results);
+            });
+        } else {
+            const term = positive_terms.shift();
+            get_galleryids_for_query(term).then(results => {
+                resolve(results);
+            });
+        }
+    }).then(() => { //positive results
+        return Promise.all(positive_terms.map(term => {
+            return new Promise((resolve, reject) => {
+                get_galleryids_for_query(term).then(new_results => {
+                    const new_results_set = new Set(new_results);
+                    results = results.filter(galleryid => new_results_set.has(galleryid));
+                    resolve();
+                })
+            });
+        }));
+    }).then(() => { //negative results
+        return Promise.all(negative_terms.map(term => {
+            return new Promise((resolve, reject) => {
+                get_galleryids_for_query(term).then(new_results => {
+                    const new_results_set = new Set(new_results);
+                    results = results.filter(galleryid => !new_results_set.has(galleryid));
+                    resolve();
+                });
+            });
+        }));
+    }).then(() => {
+        const final_results_length = results.length;
+        $('#number-of-results').html(final_results_length);
+        if (!final_results_length) {
+            hide_loading();
+            $('.gallery-content').html($('#no-results-content').html());
+        } else {
+            put_results_on_page();
+        }
+    });
+}
+
+function replace_jpg_tn(tn) {
+    if (!tn.startsWith('https')) tn = `https:${tn}`;
+    if (tn.endsWith('jpg')) tn = tn.replace('bigtn', 'webpbigtn').replace(/jpg$/, 'webp');
+
+    return tn;
+}
