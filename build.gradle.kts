0f2336eccf509945ba422d288659e1e249fcaa3e
tom5079 <7948651+tom5079@users.noreply.github.com>
2025-02-23 16:34:53 -0800

    migrate to kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.gms.oss.licenses) apply false
+    alias(libs.plugins.gms.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.perf) apply false
+}
